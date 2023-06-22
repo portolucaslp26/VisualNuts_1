b1eed39501308d26fac46e21c9a56252fd7d244a
Lucas Porto <porto.lucas.lp@gmail.com>
2023-06-22 09:34:02 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int limit = 100;
+
+        for (int i = 1; i <= limit; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Visual Nuts");
+            } else if (i % 3 == 0) {
+                System.out.println("Visual");
+            } else if (i % 5 == 0) {
+                System.out.println("Nuts");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
