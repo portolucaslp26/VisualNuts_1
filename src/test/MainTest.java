34b13ded4d243740b5037037bf55931576e3469c
Lucas Porto <68308175+portolucaslp26@users.noreply.github.com>
2023-06-23 13:31:26 -0300

    Add files via upload

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -11,20 +11,15 @@ public class MainTest {
 
     @Test
     public void testMain() {
-        // Redireciona a saída padrão para um fluxo de saída em memória
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
-        // Executa o método main
         Main.main(new String[0]);
 
-        // Obtém a saída do sistema e remove os espaços em branco extras
         String output = outputStream.toString().trim();
 
-        // Restaura a saída padrão
         System.setOut(System.out);
 
-        // Define a saída esperada com os separadores de linha e linhas em branco adicionados
         StringBuilder expectedOutputBuilder = new StringBuilder();
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
@@ -40,7 +35,6 @@ public class MainTest {
         }
         String expectedOutput = expectedOutputBuilder.toString().trim();
 
-        // Verifica se a saída está correta
         assertEquals(expectedOutput, output);
     }
 }
